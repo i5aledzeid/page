85f8d2513ee0d2aeedcd33b88a23f0127499cb3b
Khaled Zeid <107222642+i5aledzeid@users.noreply.github.com>
2022-07-26 11:05:38 +0200

    Add files via upload

--- php/connect.php
+++ php/connect.php
@@ -1,11 +1,23 @@
 <?php
 $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
-$lastName = $_POST['lastName'];
-$lastName = $_POST['lastName'];
-$lastName = $_POST['lastName'];
-$lastName = $_POST['lastName'];
-$lastName = $_POST['lastName'];
-$lastName = $_POST['lastName'];
-$lastName = $_POST['lastName'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$number = $_POST['number'];
+
+//Database connection
+$conn = new mysqli('localhost', 'root', '', tes);
+if ($conn -> connect_error) {
+    die('Connection Failed: '.$conn- connect_error);
+}
+else {
+    $stmt = $conn -> prepare("insert into Account(firstName, lastName, gender, email, password, number)
+        value(?, ?, ?, ?, ?, ?)");
+    $stmt -> bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+    $stmt -> execute();
+    echo "register successfully..."
+    $stmt -> close();
+    $conn -> close();
+}
 ?>
